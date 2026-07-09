d669bb0553f2b8bcd661a2009877b7788eb39cfe
Charles M <charlesmariajoesph@gmail.com>
2026-07-09 15:46:05 +0530

    scope

---
+++ JAVASCRIPT/blockScope.js
@@ -0,0 +1,32 @@
+// var a = 10;
+// {} // -> this is called Block 
+// {
+//     var a = 100;
+//     var a = 99
+//   let b = 2;
+//   const c = 3;
+//   console.log(a);
+//   console.log(b);
+//   console.log(c);
+// }
+// console.log(a); // 100
+// console.log(b); // not defined - block scop and different memory
+// console.log(c); // not defined - block scop and different memory
+// both are pointing same memory // shadowing
+
+let b = 10; // different memory 
+{} // -> this is called Block 
+{
+    var a = 100;
+    // let b = 2;
+    let b = 99 // block scope
+  const c = 3;
+  console.log(a);
+  console.log(b);
+  console.log(c);
+}
+console.log(b); // 10 
+// 100 ,99 , 3 ,10  
+
+// same for const 
+
