0ffb89692c1b8534ece6b21f7d2838b9820ac53d
Charles M <charlesmariajoesph@gmail.com>
2026-07-09 15:00:01 +0530

    TDZ

---
+++ JAVASCRIPT/temporalDeadZone.js
@@ -0,0 +1,12 @@
+// console.log(a)
+// const a = 1000;
+
+// backGround -> its create memory but in different place untill init that is called TDZ 
+// ReferenceError cannot access a 
+
+console.log(a)
+
+// now same ReferenceError but not defined 
+
+
+// Difference is TDZ 
