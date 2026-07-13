812156d1c5e17b694860795261b5fad2f6f2d77e
Charles M <charlesmariajoesph@gmail.com>
2026-07-13 23:06:44 +0530

    [event loop ]

---
+++ JAVASCRIPT/learning.js
@@ -0,0 +1,7 @@
+// Today LEarnt Event Loop
+//Browser
+// Web api
+
+// That setTimeout , Dom , fetch ,console ,location all are browser feature
+
+// event Loop check if the call stack is empty are not if its empty it will check the callback queue and push it but we have catch there we have microtask queue and that is higher pritory than callback queue so it will be pushed to call stack
