16d442f0eec9991cb5e835682721ce8a47a95f98
Charles M <charlesmariajoesph@gmail.com>
2026-07-14 23:58:50 +0530

    binary pratice

---
+++ DSA/binarySearch.js
@@ -0,0 +1,11 @@
+const arr = [2, 5, 8, 12, 16, 23, 38];
+let target = 16;
+let left = 0;
+let mid = math.floor((left + right) / 2);
+let right = arr.length - 1;
+while (left <= right) {
+    if (arr[left] > target) {
+        
+        
+    }
+}
